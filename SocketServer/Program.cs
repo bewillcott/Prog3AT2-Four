ecde4790d93aa290b5dd7326afb16096350a1201
bewillcott <bw.opensource@yahoo.com>
2021-10-28 12:24:33 +0800

    Initial test Chat socket server

--- SocketServer/Program.cs
+++ SocketServer/Program.cs
@@ -28,12 +28,134 @@
 namespace SocketServer
 {
     using System;
+    using System.IO;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+
+    using static Common.Constants;
 
     internal class Program
     {
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        private void StartServer()
+        {
+            IPAddress ipAddress = Dns.GetHostEntry(Dns.GetHostName()).AddressList[0];
+            IPEndPoint endPoint = new(ipAddress, SERVER_PORT);
+            TcpListener serverSocket = null;
+
+            try
+            {
+                // Create a socket that will use TCP protocol
+                serverSocket = new(endPoint);
+
+                // Specify how many requests a Socket can listen before it gives Server busy response.
+                // We will listen 10 requests at a time
+                serverSocket.Server.Listen(10);
+                serverSocket.Start();
+
+                Log("\n%1$s", DOUBLE_LINE);
+                Log("%1$sJava3 AT2 Four - Socket Server (%2$s)", TITLE_INDENT, VERSION);
+                Log("%1$s\n", DOUBLE_LINE);
+                Log("Server is listening on port #%1$d", ((IPEndPoint)serverSocket.Server.LocalEndPoint).Port.ToString());
+                Log("%1$s\n", LINE);
+
+                bool keepServerAlive = true;
+                int sessionNumber = 0;
+
+                while (keepServerAlive)
+                {
+                    using (TcpClient clientSocket = serverSocket.AcceptTcpClient())
+                    {
+                        // wait, listen and accept connection
+                        sessionNumber++;
+                        String clientHostName = ((IPEndPoint)serverSocket.Server.RemoteEndPoint).Address.ToString(); // client name
+                        int clientPortNumber = ((IPEndPoint)serverSocket.Server.RemoteEndPoint).Port; // port used
+
+                        Log("[%1$d] Connected from %2$s on #%3$d", sessionNumber, clientHostName, clientPortNumber);
+
+                        try
+                        {
+                            // input stream from client
+                            using (StreamReader inStream = new(clientSocket.GetStream()))
+                            // output stream to client
+                            using (StreamWriter outStream = new(clientSocket.GetStream()))
+                            {
+                                bool sessionOpen = true;
+
+                                // Notify client of connection success.
+                                String msg = "[" + sessionNumber + "] You have connect to Chat server " + VERSION + "";
+                                outStream.WriteLine(msg); // send a message to client
+
+                                while (sessionOpen)
+                                { // chatting loop
+                                    String inLine = inStream.ReadLine(); // read a line from client
+                                    Log("[%1$d] Received from client: %2$s", sessionNumber, inLine);
+
+                                    if (inLine == null)
+                                    {
+                                        Log("[%1$d] Client disconnected uncleanly!", sessionNumber);
+                                        Log("%1$s\n", LINE);
+                                        sessionOpen = false;
+                                    }
+                                    else
+                                    {
+                                        switch (inLine)
+                                        {
+                                            // if the client sends disconnect string, then stop
+                                            case DISCONNECT_SESSION:
+                                                {
+                                                    Log("[%1$d] Client closed session.", sessionNumber);
+                                                    Log("%1$s\n", LINE);
+                                                    sessionOpen = false;
+                                                    break;
+                                                }
+
+                                            // if the client sends terminate server string, then shutdown
+                                            case TERMINATE_SERVER:
+                                                {
+                                                    Log("[%1$d] Client Terminated Server!!!", sessionNumber);
+                                                    Log("%1$s\n", DOUBLE_LINE);
+                                                    sessionOpen = false;
+
+                                                    // Stop server
+                                                    keepServerAlive = false;
+                                                    break;
+                                                }
+
+                                            default:
+                                                {
+                                                    break;
+                                                }
+                                        }
+
+                                        // Reply to client
+                                        String outLine = "[" + sessionNumber + "] You said '" + inLine + "'";
+                                        outStream.WriteLine(outLine); // send a message to client
+                                    }
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.ToString());
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                if (serverSocket != null)
+                    serverSocket.Stop();
+            }
         }
     }
 }
